1b8db3cbcae7581e4a5e4310cc9a64f66a698288
Igor <igor.klemenski@gmail.com>
2016-03-14 11:32:03 +0100

    add reset functionality

--- Pong.py
+++ Pong.py
@@ -1,20 +1,36 @@
-import pygame, sys, time, COMMONS, os
+import pygame, sys, time, COMMONS, os, random
 import copy
 from Ball import Ball
 import math
 from Paddle import Paddle
 from pygame.locals import *
 
-def run(ball, paddle_1, paddle_2, disk_hole):
+def run(disk_hole):
 
     pygame.init()
     pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP])
+    r             = 0
+    center        = [COMMONS.WINDOWWIDTH/2, COMMONS.WINDOWHEIGHT/2]
+    radius        = 15
+    ball_vels     = [[3,1], [2,1], [-3,1], [3,-1], [-3,-1], [-2,-2], [-2,1]]
+    ball_vel      = random.choice(ball_vels)
+    win_banner_pos = {'left': [100, 50], 'right': [458, 50]}
+    paddle_speed  = 7
+    paddle_height = 100
+    paddle_width  = 10
+    ball          = Ball(center, radius, ball_vel, COMMONS.REDDISH)
+    paddle_1      = Paddle(0, 150, paddle_width, paddle_height, COMMONS.BLUEISH, paddle_speed, ball)
+    paddle_2      = Paddle(COMMONS.WINDOWWIDTH - paddle_width, 150, paddle_width, paddle_height, COMMONS.BLUEISH, paddle_speed, ball)
     dirty_rectangles = [ball.get_rect(), paddle_1.get_rect(), paddle_2.get_rect()]
     score_Font = pygame.font.SysFont("Verdana", 35)
     spaces = "                            " # dirty hack to display both scores at once (to avoid double blitting the score Font objects)
     windowSurface = pygame.display.set_mode((COMMONS.WINDOWWIDTH, COMMONS.WINDOWHEIGHT), DOUBLEBUF, 32)
     pygame.display.set_caption('Pong')
-    mainCLock 	  = pygame.time.Clock()
+    pygame.draw.circle(windowSurface, ball.color, (ball.x, ball.y), ball.radius)
+    pygame.draw.circle(windowSurface, COMMONS.WHITE, (ball.x, ball.y), ball.radius + 2, ball.radius - disk_hole)
+    pygame.display.update()
+    time.sleep(1)        
+    #mainCLock 	  = pygame.time.Clock()
 
     ball_copy 	  = copy.deepcopy(ball)
     scoreLeft 	  = scoreRight = 0
@@ -22,6 +38,7 @@ def run(ball, paddle_1, paddle_2, disk_hole):
     while max(scoreLeft, scoreRight) < 3:
         in_game = True
         paddle_1_down = paddle_1_up = paddle_2_down = paddle_2_up = False
+        mainCLock     = pygame.time.Clock()
         
         while in_game == True:
             for event in pygame.event.get():
@@ -35,6 +52,11 @@ def run(ball, paddle_1, paddle_2, disk_hole):
                         paddle_1_down = True
                     elif event.key == ord('w'):
                         paddle_1_up = True
+                    elif event.key == ord('r'):
+                        scoreRight = scoreLeft = 0
+                        ball.x = COMMONS.WINDOWWIDTH/2
+                        ball.y = COMMONS.WINDOWHEIGHT/2
+                        ball.vel  =random.choice(ball_vels)
                     elif event.key == ord('q'):
                         pygame.quit()
                         sys.exit()
@@ -68,16 +90,20 @@ def run(ball, paddle_1, paddle_2, disk_hole):
             if ball.x - ball.radius <= 0: 
                 if ball.y >= paddle_1.y and ball.y <= paddle_1.y + paddle_1.height:
                     ball.vel[0] = -int(math.floor(1.1*ball.vel[0]))
+                    print ball.vel[0]
                 else:
                     scoreRight += 1
                     in_game = False
                     
+                    
             elif ball.x + ball.radius >= COMMONS.WINDOWWIDTH:
                 if ball.y >= paddle_2.y and ball.y <= paddle_2.y + paddle_2.height:
-                    ball.vel[0] = -int(math.floor(1.1*ball.vel[0]))
+                    ball.vel[0] = int(math.floor(-1.1*ball.vel[0]))
+                    print ball.vel[0]
                 else:
                     scoreLeft += 1
                     in_game = False
+                    
                                 
             windowSurface.fill(COMMONS.BLACK)
             
@@ -100,30 +126,43 @@ def run(ball, paddle_1, paddle_2, disk_hole):
             windowSurface.blit(scoreSurface, (150,80))
            
             pygame.display.update()
+            #mainCLock.tick_busy_loop(100)
             mainCLock.tick(100)
+        #time.sleep(0.5)
         
-        ball = copy.deepcopy(ball_copy)
-        time.sleep(1) 
+        #ball = copy.deepcopy(ball_copy)
+        ball.x = ball_copy.x
+        ball.y = ball_copy.y
+        ball.vel = random.choice(ball_vels)
+        pygame.time.wait(500)
 
     win_Surface = pygame.font.SysFont("Verdana", 30).render("You win", False, COMMONS.WHITE)
-    windowSurface.blit(win_Surface, (100,50))
+    if scoreLeft > scoreRight:
+        winner = 'left' 
+    else:
+        winner = 'right'
+    windowSurface.blit(win_Surface, win_banner_pos[winner])
     pygame.display.update()     
  
-        
+
+def super_run(disk_hole):
+
+    run(disk_hole)
+    while True:
+        for event in pygame.event.get():  
+            if event.type == KEYDOWN:
+                if event.key == ord('r'):
+                    run(disk_hole)
+                elif event.key == ord('q'):
+                    pygame.quit()
+                    sys.exit()
+        time.sleep(1)
+
+
 def main():
 
-    r 		 	  = 0
-    center 		  = [COMMONS.WINDOWWIDTH/2, COMMONS.WINDOWHEIGHT/2]
-    radius 		  = 15
-    ball_vel	  = [4,2]
-    disk_hole	  = 10
-    paddle_speed  = 7
-    paddle_height = 100
-    paddle_width  = 10
-    ball 	 	  = Ball(center, radius, ball_vel, COMMONS.REDDISH)
-    paddle_1 	  = Paddle(0, 150, paddle_width, paddle_height, COMMONS.BLUEISH, paddle_speed, ball)
-    paddle_2 	  = Paddle(COMMONS.WINDOWWIDTH - paddle_width, 150, paddle_width, paddle_height, COMMONS.BLUEISH, paddle_speed, ball)
-    run(ball, paddle_1, paddle_2, disk_hole)
+    disk_hole     = 10
+    super_run(disk_hole)
     os.system("pause")
     pygame.quit()
     return 0
